81a4194b2384199f88bfbdb04660a04b80c4094c
RobbyHazwaRizki <robbyhazwarizky27@gmail.com>
2023-10-30 15:06:48 +0700

    menambahkan Contoh1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 Extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Perkenalkan <h1>";
+        echo " Nama Saya Robby HazwaRizki Wijaya
+               Saya Tinggal di Kampung Karang Tengah
+               Olahraga yang saya sukai adalah Basket";
+    }
+    
+}
